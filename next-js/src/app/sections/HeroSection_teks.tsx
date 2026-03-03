9b6a8f81db26c261b4e02310c25c206d92ee7c1d
galihsuks <galih8.4.2001@gmail.com>
2026-03-03 13:05:55 +0700

    coba-staging

--- next-js/src/app/sections/HeroSection_teks.tsx
+++ next-js/src/app/sections/HeroSection_teks.tsx
@@ -47,7 +47,7 @@ export default function HeroSectionTeks() {
 
     return (
         <>
-            <p className="text-xs md:text-sm">{teks} Developer</p>
+            <p className="text-xs md:text-sm">{teks} Developer Anjay</p>
         </>
     );
 }
